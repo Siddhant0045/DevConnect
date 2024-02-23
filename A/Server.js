f54713848d242740eaa70cd738d4b476e8f4f510
VikasGoudar <avikashasappeared@gmail.com>
2024-02-23 10:25:16 +0530

    pics

--- A/Server.js
+++ A/Server.js
@@ -38,7 +38,7 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render("login.ejs");
 });
 
 app.get('/landing_page', (req, res) => {
@@ -58,38 +58,6 @@ app.post('/findprojectpage', (req, res) => {
 app.post('/myprojects', (req, res) => {
     res.render("myprojects.ejs");
 });
-
-app.post('/LogiN', (req, res) => {
-    res.render("login.ejs");
-});
-
-
-app.post('/login', (req, res) => {
-    // Simulate authentication logic'
-         
-    const { username, password } = req.body;
-    
-    
-    // Check if username and password are correct
-    if (username === 'user' && password === 'password') {
-      // If correct, send a success response
-      //res.redirect(__dirname + '/public/landing_page.html');
-      res.render("landing_page.ejs");
-      //res.status(200).json({ message: 'valid username or password' });
-      
-      
-    } else {
-      // If incorrect, send an error response
-    //   res.status(401).json({ message: 'Invalid username or password' });
-    }
-  });
-
-  
-
-  
-
-    
-
   const port = 3000 // Port we will listen on
 
 // Function to listen on the port
